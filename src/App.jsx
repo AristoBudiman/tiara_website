31a55a7df88436134e956a465bab80f929b278ee
AristoBudiman <aristobudimannew@gmail.com>
2025-06-01 18:48:42 +0700

    first commit + config tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@material-tailwind/react";
+
+const App = () => (
+  <div className="p-10">
+    <Button>Click me</Button>
+    <div className="bg-red-500 text-white p-4 mt-4">Hello Tailwind</div>
+  </div>
+);
+
+export default App;
