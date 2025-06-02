179fdb0a0425de5e2880a2efc98cca3443f2b077
AristoBudiman <aristobudimannew@gmail.com>
2025-06-02 23:32:42 +0700

    add Login Function, fix navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,74 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import SearchBar from "../searchBar/SearchBar";
-import logo from "../../assets/logo.png"; 
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+    // get user from localStorage 
+    const user = JSON.parse(localStorage.getItem('users'));
+
+    // navigate 
+    const navigate = useNavigate();
+
+    // logout function 
+    const logout = () => {
+        localStorage.clear('users');
+        navigate("/login")
+    }
+
+    // navList Data
+    const navList = (
+        <ul className="flex items-center space-x-6 text-[#F0BB78] font-semibold ">
+            {/* Home */}
+            <li>
+                <Link to={'/'}>Home</Link>
+            </li>
+            {/* Cart */}
+            <li>
+                <Link to="/cart" className="flex items-center space-x-1">
+                    <FaShoppingCart />
+                </Link>
+            </li>
+            {/* Orders */}
+            <li>
+                <Link to="/orders">Orders</Link>
+            </li>
+            {/* About Us */}
+            <li>
+                <Link to="/about">About Us</Link>
+            </li>
+            {/* All Product
+            <li>
+                <Link to={'/allproduct'}>All Product</Link>
+            </li> */}
+            {/* Signup */}
+            {!user ? <li>
+                <Link to={'/signup'}>Signup</Link>
+            </li> : ""}
+            {/* Login */}
+            {!user ? <li>
+                <Link to={'/login'}>Login</Link>
+            </li> : ""}
+            {/* User */}
+            {user?.role === "user" && <li>
+                <Link to={'/user-dashboard'}>User</Link>
+            </li>}
+            {/* Admin */}
+            {user?.role === "admin" && <li>
+                <Link to={'/admin-dashboard'}>Admin</Link>
+            </li>}
+            {/* logout */}
+            {user && <li className=" cursor-pointer" onClick={logout}>Logout
+            </li>}
+            {/* Account
+            <li>
+                <Link to={user ? "/user-dashboard" : "/login"} className="flex items-center space-x-1">
+                    <FaUser />
+                    <span>Account</span>
+                </Link>
+            </li> */}
+        </ul>
+    )
     return (
         <nav className="bg-[#FFFFFF] shadow-md sticky top-0 z-50">
             <div className="max-w-7xl mx-auto flex items-center justify-between py-0 px-4">
@@ -18,16 +83,8 @@ const Navbar = () => {
                 </div>
 
                 {/* Nav Items */}
-                <div className="flex items-center space-x-6 text-[#F0BB78] font-semibold">
-                    <Link to="/cart" className="flex items-center space-x-1">
-                        <FaShoppingCart />
-                    </Link>
-                    <Link to="/orders">Orders</Link>
-                    <Link to="/about">About Us</Link>
-                    <Link to="/user-dashboard" className="flex items-center space-x-1">
-                        <FaUser />
-                        <span>Account</span>
-                    </Link>
+                <div>
+                    {navList}
                 </div>
             </div>
         </nav>
--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,14 +1,77 @@
-/* eslint-disable react/no-unescaped-entities */
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 import logo from "../../assets/logo.png"; 
+import myContext from "../../context/myContext";
+import toast from "react-hot-toast";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, fireDB } from "../../firebase/FirebaseConfig";
+import Loader from "../../components/loader/Loader";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 
 const Login = () => {
+
+  const context = useContext(myContext);
+  const { loading, setLoading } = context;
+
+  // navigate 
+  const navigate = useNavigate();
+
+  // User Signup State 
+  const [userLogin, setUserLogin] = useState({
+      email: "",
+      password: ""
+  });
+
   const [showPassword, setShowPassword] = useState(false);
+
+  const userLoginFunction = async () => {
+    // validation 
+    if (userLogin.email === "" || userLogin.password === "") {
+        toast.error("All Fields are required")
+    }
+
+    setLoading(true);
+    try {
+        const users = await signInWithEmailAndPassword(auth, userLogin.email, userLogin.password);
+        // console.log(users.user)
+
+        try {
+            const q = query(
+                collection(fireDB, "users"),
+                where('uid', '==', users?.user?.uid)
+            );
+            const data = onSnapshot(q, (QuerySnapshot) => {
+                let user;
+                QuerySnapshot.forEach((doc) => user = doc.data());
+                localStorage.setItem("users", JSON.stringify(user) )
+                setUserLogin({
+                    email: "",
+                    password: ""
+                })
+                toast.success("Login Successfully");
+                setLoading(false);
+                if(user.role === "user") {
+                    navigate('/user-dashboard');
+                }else{
+                    navigate('/admin-dashboard');
+                }
+            });
+            return () => data;
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    } catch (error) {
+        console.log(error);
+        setLoading(false);
+        toast.error("Login Failed");
+    }
+  }
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-[#FFF0DC]">
+      {loading && <Loader/>}
       <div className="bg-[#FFFFFF] w-[400px] rounded-2xl shadow-lg p-8">
         {/* Logo */}
         <div className="flex flex-col items-center mb-6">
@@ -31,6 +94,13 @@ const Login = () => {
           <input
             type="email"
             placeholder="yours@gmail.com"
+            value={userLogin.email}
+            onChange={(e) => {
+                setUserLogin({
+                    ...userLogin,
+                    email: e.target.value
+                })
+            }}
             className="w-full bg-[#FFFDF9] border border-[#000000] rounded-md px-3 py-2 placeholder-[#D9D9D9] outline-none"
           />
         </div>
@@ -43,6 +113,13 @@ const Login = () => {
           <input
             type={showPassword ? "text" : "password"}
             placeholder="Your password"
+            value={userLogin.password}
+            onChange={(e) => {
+                setUserLogin({
+                    ...userLogin,
+                    password: e.target.value
+                })
+            }}
             className="w-full bg-[#FFFDF9] border border-[#000000] rounded-md px-3 py-2 pr-10 placeholder-[#D9D9D9] outline-none"
           />
           <button
@@ -63,6 +140,7 @@ const Login = () => {
 
         {/* Login Button */}
         <button
+          onClick={userLoginFunction}
           type="button"
           className="w-full mt-2 bg-[#F0BB78] hover:bg-[#e6a95d] text-[#543A14] font-semibold py-2 rounded-md"
         >
