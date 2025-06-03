70accfe5d22b13afd25a3a71a6b83864c91cf293
AvecenaHidayat <avecenahidayat@student.uns.ac.id>
2025-06-03 20:22:33 +0700

    fix admindashboard icon

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -3,6 +3,9 @@ import ProductDetail from '../../components/admin/ProductDetail';
 import UserDetail from '../../components/admin/UserDetail';
 import logo from "../../assets/logo.png"; 
 import myContext from '../../context/myContext';
+import { FiShoppingBag } from "react-icons/fi";      // Feather icon
+import { FaListOl, FaUsers } from "react-icons/fa";  // Font Awesome icons
+
 
 const AdminDashboard = () => {
     const user = JSON.parse(localStorage.getItem('users'));
@@ -64,89 +67,41 @@ const AdminDashboard = () => {
                             <div className="flex flex-wrap -m-4 pt-4 text-center justify-center">
                                 {/* Total Products */}
                                 <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
-                                    <div className=" border bg-[#FFF0DC] px-4 py-3 rounded-xl" >
-                                        <div className="text-[#543A14] w-12 h-12 mb-3 inline-block" >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width={50}
-                                                height={50}
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                                stroke="currentColor"
-                                                strokeWidth={2}
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                className="lucide lucide-shopping-basket"
-                                            >
-                                                <path d="m5 11 4-7" />
-                                                <path d="m19 11-4-7" />
-                                                <path d="M2 11h20" />
-                                                <path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8c.9 0 1.8-.7 2-1.6l1.7-7.4" />
-                                                <path d="m9 11 1 9" />
-                                                <path d="M4.5 15.5h15" />
-                                                <path d="m15 11-1 9" />
-                                            </svg>
-
+                                    <div className="border bg-[#FFF0DC] px-4 py-3 rounded-xl flex justify-center items-center gap-4">
+                                        <div className="text-[#543A14] w-12 h-12">
+                                            <FiShoppingBag size={40} />
                                         </div>
-                                        <h2 className="title-font font-medium text-3xl text-[#543A14] fonts1" >{getAllProduct.length}</h2>
-                                        <p className=" text-[#543A14]  font-bold" >Total Products</p>
+                                        <div>
+                                            <h2 className="title-font font-medium text-3xl text-[#543A14] fonts1">{getAllProduct.length}</h2>
+                                            <p className="text-[#543A14] font-bold">Total Products</p>
+                                        </div>
                                     </div>
                                 </div>
 
+
                                 {/* Total Order  */}
                                 <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-                                    <div className=" border bg-[#FFF0DC] px-4 py-3 rounded-xl" >
-                                        <div className="text-[#543A14] w-12 h-12 mb-3 inline-block" >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width={50}
-                                                height={50}
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                                stroke="currentColor"
-                                                strokeWidth={2}
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                className="lucide lucide-list-ordered"
-                                            >
-                                                <line x1={10} x2={21} y1={6} y2={6} />
-                                                <line x1={10} x2={21} y1={12} y2={12} />
-                                                <line x1={10} x2={21} y1={18} y2={18} />
-                                                <path d="M4 6h1v4" />
-                                                <path d="M4 10h2" />
-                                                <path d="M6 18H4c0-1 2-2 2-3s-1-1.5-2-1" />
-                                            </svg>
+                                    <div className=" border bg-[#FFF0DC] px-4 py-3 rounded-xl flex justify-center items-center gap-4" >
+                                        <div className="text-[#543A14] w-12 h-12" >
+                                            <FaListOl size={40} />
                                         </div>
-                                        <h2 className="title-font font-medium text-3xl text-[#543A14] fonts1" >10</h2>
-                                        <p className=" text-[#543A14]  font-bold" >Total Order</p>
+                                        <div>
+                                            <h2 className="title-font font-medium text-3xl text-[#543A14] fonts1" >10</h2>
+                                            <p className=" text-[#543A14]  font-bold" >Total Order</p>
+                                        </div>
                                     </div>
                                 </div>
 
                                 {/* Total User  */}
                                 <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
-                                    <div className=" border bg-[#FFF0DC] px-4 py-3 rounded-xl" >
-                                        <div className="text-[#543A14] w-12 h-12 mb-3 inline-block" >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width={50}
-                                                height={50}
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                                stroke="currentColor"
-                                                strokeWidth={2}
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                className="lucide lucide-users"
-                                            >
-                                                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                                                <circle cx={9} cy={7} r={4} />
-                                                <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                                                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                            </svg>
-
+                                    <div className=" border bg-[#FFF0DC] px-4 py-3 rounded-xl flex justify-center items-center gap-4" >
+                                        <div className="text-[#543A14] w-12 h-12 inline-block" >
+                                            <FaUsers size={40} />
                                         </div>
-                                        <h2 className="title-font font-medium text-3xl text-[#543A14] fonts1" >10</h2>
-                                        <p className=" text-[#543A14]  font-bold" >Total Order</p>
+                                        <div>
+                                            <h2 className="title-font font-medium text-3xl text-[#543A14] fonts1" >10</h2>
+                                            <p className=" text-[#543A14]  font-bold" >Total User</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
