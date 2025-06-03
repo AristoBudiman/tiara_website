34ec757707eaa82af1268c1c359ff68ac362baea
AristoBudiman <aristobudimannew@gmail.com>
2025-06-03 12:50:30 +0700

    admin view product

--- src/context/myState.jsx
+++ src/context/myState.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MyContext from './myContext';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { fireDB } from '../firebase/FirebaseConfig';
 
-function MyState({children}) {
+function MyState({ children }) {
+    // Loading State 
     const [loading, setLoading] = useState(false);
+
+    // User State
+    const [getAllProduct, setGetAllProduct] = useState([]);
+
+    const getAllProductFunction = async () => {
+        setLoading(true);
+        try {
+            const q = query(
+                collection(fireDB, 'data', 'stock', 'products'),
+                orderBy('actualPrice')
+            );
+            const data = onSnapshot(q, (QuerySnapshot) => {
+                let productArray = [];
+                QuerySnapshot.forEach((doc) => {
+                    productArray.push({ ...doc.data(), id: doc.id });
+                });
+                setGetAllProduct(productArray);
+                setLoading(false);
+            });
+            return () => data;
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getAllProductFunction();
+    }, []);
     return (
         <MyContext.Provider value={{
             loading,
-            setLoading
+            setLoading,
+            getAllProduct
         }}>
-        {children}
+            {children}
         </MyContext.Provider>
     )
 }
