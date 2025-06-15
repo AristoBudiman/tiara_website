11451e6d96977c46b7560851b3a4f4c9a60ff0bc
AristoBudiman <aristobudimannew@gmail.com>
2025-06-15 17:46:42 +0700

    fix login

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
 
       // Redirect berdasarkan role
       if (userData.role === "user") {
-        navigate('/user-dashboard');
+        navigate('/');
       } else {
         navigate('/admin-dashboard');
       }
